8dec1aab68dee2c67f621a2c627a492fd10570c1
David Poblador i Garcia <david@poblador.com>
2024-12-13 13:22:31 +0100

    linter warnings

--- src/whisperx/__init__.py
+++ src/whisperx/__init__.py
@@ -1,4 +1,13 @@
-from .transcribe import load_model
-from .alignment import load_align_model, align
+from .alignment import align, load_align_model
 from .audio import load_audio
-from .diarize import assign_word_speakers, DiarizationPipeline
+from .diarize import DiarizationPipeline, assign_word_speakers
+from .transcribe import load_model
+
+__all__ = [
+    "load_model",
+    "load_align_model",
+    "align",
+    "load_audio",
+    "assign_word_speakers",
+    "DiarizationPipeline",
+]
